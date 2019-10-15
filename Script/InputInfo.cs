e9cd63dafff271f92a299861bee78f1bf3bb531e
Icarus <1375400884@qq.com>
2019-10-15 16:56:59 +0800

    !AD 改名 KeyInfo -> InputInfo

--- Script/InputInfo.cs
+++ Script/InputInfo.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 
 namespace CabinIcarus.Joystick
 {
-    [Serializable]
-    public class KeyInfo
+    public enum InputState
     {
+        None,
+        Hold,
+        Up,
+    }
+    [Serializable]
+    public class InputInfo
+    {
+        public bool Enable = true;
         public KeyCode PrimaryKey;
         public KeyCode Secondary;
-        
-        public bool Enable;
 
+        public InputState State { get; private set; }
         public bool Hold { get; private set; }
         public bool Down { get; private set;}
         public bool Up { get; private set;}
@@ -27,6 +33,18 @@ namespace CabinIcarus.Joystick
             Down = Input.GetKeyDown(PrimaryKey) | Input.GetKeyDown(Secondary);
             Hold = Input.GetKey(PrimaryKey) | Input.GetKey(Secondary);
             Up = Input.GetKeyUp(PrimaryKey) | Input.GetKeyUp(Secondary);
+
+            if (Hold)
+            {
+                State = InputState.Hold;
+            }else if (Up)
+            {
+                State = InputState.Up;
+            }
+            else
+            {
+                State = InputState.None;
+            }
         }
     }
 }

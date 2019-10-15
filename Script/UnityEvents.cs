35dca8f74e52a2b2d7ba938a12a8776c6a9e8ac7
Icarus <1375400884@qq.com>
2019-10-15 16:57:20 +0800

    !A UnityEvents

---
+++ Script/UnityEvents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine.Events;
+
+namespace CabinIcarus.Joystick.Evetns
+{
+   [Serializable]
+   public class InputDownEvent:UnityEvent{}
+   
+   [Serializable]
+   public class InputUpEvent:UnityEvent{}
+   
+   [Serializable]
+   public class InputHold : UnityEvent<float>{}
+}

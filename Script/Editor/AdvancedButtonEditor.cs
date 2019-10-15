69b15dcedd8073093f76afb03d5387738c8e1066
Icarus <1375400884@qq.com>
2019-10-15 17:00:15 +0800

    !R AdvancedButton Editor 不在继承`ButtonEditor`而是继承`SelectableEditor`

--- Script/Editor/AdvancedButtonEditor.cs
+++ Script/Editor/AdvancedButtonEditor.cs
@@ -9,19 +9,27 @@ namespace CabinIcarus.Joystick.Editor
 {
     [CustomEditor(typeof(AdvancedButton),true)]
     [CanEditMultipleObjects]
-    public class AdvancedButtonEditor : ButtonEditor
+    public class AdvancedButtonEditor : SelectableEditor
     {
-        SerializedProperty _keysProperty;
-        SerializedProperty _isHoldProperty;
-        SerializedProperty _holdTriggerTimeProperty;
-        SerializedProperty _isDownProperty;
+        SerializedProperty _InputBindProperty;
+        SerializedProperty _UseDownTriggerProperty;
+        SerializedProperty _HoldTriggerIntervalProperty;
+        SerializedProperty _OnDownProperty;
+        SerializedProperty _OnHoldProperty;
+        SerializedProperty _OnUPProperty;
+        SerializedProperty _onClickProperty;
+
         protected override void OnEnable()
         {
             base.OnEnable();
-            _keysProperty = serializedObject.FindProperty("Keys");
-            _isHoldProperty = serializedObject.FindProperty("IsHold");
-            _holdTriggerTimeProperty = serializedObject.FindProperty("HoldTriggerTime");
-            _isDownProperty = serializedObject.FindProperty("IsDownTrigger");
+            _UseDownTriggerProperty = serializedObject.FindProperty("UseDownTrigger");
+            _HoldTriggerIntervalProperty = serializedObject.FindProperty("HoldTriggerInterval");
+            _InputBindProperty = serializedObject.FindProperty("InputBind");
+            
+            _OnDownProperty = serializedObject.FindProperty("OnDown");
+            _OnHoldProperty = serializedObject.FindProperty("OnHold");
+            _OnUPProperty = serializedObject.FindProperty("OnUp");
+            _onClickProperty = serializedObject.FindProperty("m_OnClick");
         }
 
         public override void OnInspectorGUI()
@@ -32,12 +40,10 @@ namespace CabinIcarus.Joystick.Editor
                 {
                     serializedObject.Update();
                     {
-                        EditorGUILayout.PropertyField(_isDownProperty);
-                        EditorGUILayout.PropertyField(_isHoldProperty);
-                        EditorGUILayout.PropertyField(_holdTriggerTimeProperty);
+                        EditorGUILayout.PropertyField(_HoldTriggerIntervalProperty);
+                        EditorGUILayout.PropertyField(_UseDownTriggerProperty);
                         GUILayout.FlexibleSpace();
-                        
-                        EditorGUILayout.PropertyField(_keysProperty);
+                        EditorGUILayout.PropertyField(_InputBindProperty);
                     }
                     serializedObject.ApplyModifiedProperties();
                 }
@@ -46,6 +52,23 @@ namespace CabinIcarus.Joystick.Editor
             EditorGUILayout.EndVertical();
             
             base.OnInspectorGUI();
+            
+            //Event 
+            serializedObject.Update();
+            {
+                EditorGUILayout.PropertyField(_OnHoldProperty);
+
+                if (_UseDownTriggerProperty.boolValue)
+                {
+                    EditorGUILayout.PropertyField(_OnDownProperty);
+                    EditorGUILayout.PropertyField(_OnUPProperty);
+                }
+                else
+                {
+                    EditorGUILayout.PropertyField(_onClickProperty);
+                }
+            }
+            serializedObject.ApplyModifiedProperties();
         }
 
         [MenuItem("CONTEXT/Button/To Advanced Button",false)]

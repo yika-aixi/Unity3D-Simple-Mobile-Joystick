232db4643218b52871b70f96f4a765e75ba89a4c
Icarus <1375400884@qq.com>
2019-12-05 23:20:36 +0800

    !A 冷却按钮

---
+++ Script/Components/CoolingButton.cs
@@ -0,0 +1,76 @@
+﻿using CabinIcarus.Joystick.Evetns;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CabinIcarus.Joystick.Components
+{
+    public class CoolingButton : AdvancedButton
+    {
+        public float CoolingTime;
+
+        public float CurrentCoolingTime;
+
+        public Image CoolingMask;
+
+        [SerializeField]
+        private bool _coolingState;
+        
+        protected override void Awake()
+        {
+            if (CoolingMask)
+            {
+                CoolingMask.enabled = false;
+            }
+
+            if (_coolingState)
+            {
+                EnterCooling();
+            }
+        }
+
+        public void EnterCooling()
+        {
+            _coolingState = true;
+            CurrentCoolingTime = CoolingTime;
+            if (CoolingMask)
+            {
+                CoolingMask.enabled = true;
+            }
+
+            interactable = false;
+        }
+
+        public NoParEvent OnCoolingComplete = new NoParEvent();
+
+        private void FixedUpdate()
+        {
+            if (_coolingState)
+            {
+                CurrentCoolingTime -= Time.fixedDeltaTime;
+
+                if (CoolingMask)
+                {
+                    CoolingMask.fillAmount = CurrentCoolingTime / CoolingTime;
+                }
+                
+                if (CurrentCoolingTime <= 0)
+                {
+                    _complete();
+                }
+            }
+        }
+
+        private void _complete()
+        {
+            _coolingState = false;
+            
+            interactable = true;
+            if (CoolingMask)
+            {
+                CoolingMask.enabled = false;
+            }
+
+            OnCoolingComplete.Invoke();
+        }
+    }
+}
---
+++ Script/Editor/CoolingButtonEditor.cs
@@ -0,0 +1,80 @@
+﻿using CabinIcarus.Joystick.Components;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CabinIcarus.Joystick.Editor
+{
+    [CustomEditor(typeof(CoolingButton),true)]
+    [CanEditMultipleObjects]
+    public class CoolingButtonEditor:AdvancedButtonEditor
+    {
+        private SerializedProperty _coolingTime;
+
+        private SerializedProperty _coolingMask;
+
+        private SerializedProperty _onCompleteEvent;
+        
+        private SerializedProperty _coolingState;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            _coolingTime = serializedObject.FindProperty(nameof(CoolingButton.CoolingTime));
+            _coolingMask = serializedObject.FindProperty(nameof(CoolingButton.CoolingMask));
+            _onCompleteEvent = serializedObject.FindProperty(nameof(CoolingButton.OnCoolingComplete));
+            _coolingState = serializedObject.FindProperty("_coolingState");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            {
+                EditorGUILayout.PropertyField(_coolingState, new GUIContent("Play Enter Cooling?"));
+                EditorGUILayout.PropertyField(_coolingTime);
+                EditorGUILayout.PropertyField(_coolingMask);
+                if (!_coolingMask.objectReferenceValue)
+                {
+                    if (GUILayout.Button("Create Mask"))
+                    {
+                        _createMaks();
+                    }
+                }
+            }
+            serializedObject.ApplyModifiedProperties();
+            
+            base.OnInspectorGUI();
+            
+            serializedObject.Update();
+            {
+                EditorGUILayout.PropertyField(_onCompleteEvent);
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+        private const string kStandardSpritePath = "UI/Skin/UISprite.psd";
+        private void _createMaks()
+        {
+            GameObject go = new GameObject("Cooling Mask");
+            RectTransform rectTransform = go.AddComponent<RectTransform>();
+            
+            Image image = go.AddComponent<Image>();
+            image.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite>(kStandardSpritePath);
+            image.type = Image.Type.Filled;
+            image.fillMethod = Image.FillMethod.Radial360;
+            image.color = new Color(Color.black.r, Color.black.g, Color.black.b, 0.4f);
+            
+            Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+            Undo.SetTransformParent(go.transform, Selection.activeTransform, "Parent " + go.name);
+            
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.one;
+            rectTransform.anchoredPosition = Vector2.zero;
+            rectTransform.sizeDelta = Vector2.zero;
+            
+            Selection.activeGameObject = go;
+            
+            _coolingMask.objectReferenceValue = image;
+        }
+    }
+}

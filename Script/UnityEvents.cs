149029aae0eb4063bb2624175b3d682728c8010b
Icarus <1375400884@qq.com>
2019-12-05 23:20:53 +0800

    !W 一个无参的unity 事件

--- Script/UnityEvents.cs
+++ Script/UnityEvents.cs
@@ -3,6 +3,14 @@ using UnityEngine.Events;
 
 namespace CabinIcarus.Joystick.Evetns
 {
+   /// <summary>
+   /// 无参事件
+   /// </summary>
+   [Serializable]
+   public class NoParEvent : UnityEvent
+   {
+   }
+   
    [Serializable]
    public class InputDownEvent:UnityEvent{}
    
